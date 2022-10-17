5e9907682cfb0b45610cea080b3bcca383d0e3bb
Stuart Bruce <programming@brucey.me.uk>
2022-10-17 22:48:15 +0100

    Sync state between tabs

--- src/state.rs
+++ src/state.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 use yewdux::store::Store;
 
 #[derive(Clone, Default, Debug, PartialEq, Deserialize, Serialize, Store)]
-#[store(storage = "local")]
+#[store(storage = "local", storage_tab_sync)]
 pub struct State {
     pub entities: Vec<Entity>,
 }
