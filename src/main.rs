64063358613705a768039ae46b377b38dd10e0be
Stuart Bruce <programming@brucey.me.uk>
2022-10-17 22:42:56 +0100

    Add entry to stored state.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use web_sys::HtmlInputElement;
 use yew::prelude::*;
 use yewdux::prelude::*;
 
@@ -7,6 +8,38 @@ use state::{Entity, State};
 
 #[function_component(App)]
 fn app() -> Html {
+    html! {
+        <div>
+            <h1>{ "TodoMVC - Yew" }</h1>
+            <AddEntry />
+            <ListEntities />
+        </div>
+    }
+}
+
+#[function_component(AddEntry)]
+fn add_entry() -> Html {
+    let onkeypress = Dispatch::<State>::new().reduce_mut_callback_with(|s, e: KeyboardEvent| {
+        if e.key() == "Enter" {
+            let input: HtmlInputElement = e.target_unchecked_into();
+            let value = input.value();
+            input.set_value("");
+            if !value.is_empty() {
+                s.entities.push(Entity {
+                    description: value.trim().to_string(),
+                    ..Default::default()
+                })
+            }
+        }
+    });
+
+    html! {
+        <input type="text" placeholder="Add todo" {onkeypress} />
+    }
+}
+
+#[function_component(ListEntities)]
+fn list_entities() -> Html {
     let (state, dispatch) = use_store::<State>();
 
     let view_entry = |(index, entity): (usize, &Entity)| {
@@ -21,9 +54,12 @@ fn app() -> Html {
         .collect::<Html>();
 
     html! {
-        <ul>
-            { entries }
-        </ul>
+        <div>
+            <h3>{"Entries"}</h3>
+            <ul>
+                { entries }
+            </ul>
+        </div>
     }
 }
 
--- src/state.rs
+++ src/state.rs
@@ -1,13 +1,13 @@
 use serde::{Deserialize, Serialize};
 use yewdux::store::Store;
 
-#[derive(Default, Debug, PartialEq, Deserialize, Serialize, Store)]
+#[derive(Clone, Default, Debug, PartialEq, Deserialize, Serialize, Store)]
 #[store(storage = "local")]
 pub struct State {
     pub entities: Vec<Entity>,
 }
 
-#[derive(Default, Debug, PartialEq, Deserialize, Serialize, Store)]
+#[derive(Clone, Default, Debug, PartialEq, Deserialize, Serialize, Store)]
 pub struct Entity {
     pub description: String,
     pub is_completed: bool,
